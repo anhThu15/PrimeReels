b85a92fd59225ccac29cc4e9c7da301c6066e38b
ameo <iamthief1412@gmail.com>
2024-09-16 14:47:10 +0700

    setup footer và header

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -4,16 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <>
-       <div
-        class="alert alert-success"
-        role="alert"
-       >
-        <h4 class="alert-heading">Alert Heading</h4>
-        <p>Alert Content</p>
-        <hr />
-        <p class="mb-0">Alert Description</p>
-       </div>
-       
+        
     </>
   );
 }
