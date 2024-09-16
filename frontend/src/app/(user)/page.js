c4bc5730f0e07e0af993e238aaf3bda598eef919
ameo <iamthief1412@gmail.com>
2024-09-16 23:39:44 +0700

    list choice

--- frontend/src/app/(user)/page.js
+++ frontend/src/app/(user)/page.js
@@ -1,13 +1,17 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import ListChoie from "../layout/user/listchoie";
 
 export default function Home() {
   
   return (
     <>
-     <div className="container-fluid d-flex justify-content-center pt-3" style={{backgroundColor:"#283237", width:"100%", height:"83vh"}}>
-        <div className="containerVipPro font-monospace h-100 bg-nenVipPro" data-bs-theme="dark" >
-            
+     <div className="container-fluid d-flex justify-content-center pt-3" style={{backgroundColor:"#283237", width:"100%"}}>
+        <div className="containerVipPro font-monospace h-100 bg-nenVipPro row pb-5" data-bs-theme="dark" >
+          <div className=" mt-3 col font-monospace text-white">Sản Phẩm bên đây nhóe</div>
+          <div className=" mt-3 col-4 font-monospace text-white">
+              <ListChoie></ListChoie>
+          </div>
         </div>
      </div>
     </>
