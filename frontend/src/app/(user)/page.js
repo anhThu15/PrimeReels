4de9b905e8670e591211a6d4301d9c5f209e6bcb
ameo <iamthief1412@gmail.com>
2024-09-16 21:57:04 +0700

    xong header

--- frontend/src/app/(user)/page.js
+++ frontend/src/app/(user)/page.js
@@ -2,11 +2,12 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
+  
   return (
     <>
-     <div className="container-fluid " style={{backgroundColor:"#283237", width:"100%", height:"100vh"}}>
+     <div className="container-fluid " style={{backgroundColor:"#283237", width:"100%", height:"83vh"}}>
         <div className="container">
-
+            
         </div>
      </div>
     </>
--- frontend/src/app/layout/user/header.jsx
+++ frontend/src/app/layout/user/header.jsx
@@ -1,6 +1,40 @@
-import Logo from "../../../../public/images/Logo PR.svg";
+'use client'
+import { useEffect } from "react";
+import "../../../app/globals.css"
+
 
 export default function HeaderUser(){
+
+  const dangphim = `<div class="text-start">
+                        <a class="dropdown-item p-2" href="#">Phim Dài Tập </a>
+                        <a class="dropdown-item p-2" href="#">Phim Ngắn Tập</a>
+                    </div>`
+  const topphim = `<div class="d-flex flex-wrap">
+                        <a class="nav-link p-2 col-6 " href="#">Theo Ngày </a>
+                        <a class="nav-link p-2 col-6 " href="#">Theo Tháng</a>
+                        <a class="nav-link p-2 col-6 " href="#">Theo Năm</a>
+                        <a class="nav-link p-2 col-6 " href="#">Theo Mùa</a>
+                        <a class="nav-link p-2 col-6 " href="#">Theo Yêu Thích</a>
+                    </div>`
+  const theloaiphim = `<div class="d-flex flex-wrap">
+                        <a class="nav-link p-2 col-6 " href="#">Phim Hài </a>
+                        <a class="nav-link p-2 col-6 " href="#">Phim Tình Cảm</a>
+                        <a class="nav-link p-2 col-6 " href="#">Phim Kinh Dị</a>
+                        <a class="nav-link p-2 col-6 " href="#">Phim 18+</a>
+                        <a class="nav-link p-2 col-6 " href="#">Phim Hài </a>
+                        <a class="nav-link p-2 col-6 " href="#">Phim Tình Cảm</a>
+                        <a class="nav-link p-2 col-6 " href="#">Phim Kinh Dị</a>
+                        <a class="nav-link p-2 col-6 " href="#">Phim 18+</a>
+                  </div>`
+
+    useEffect(() => {
+      // Kích hoạt tooltip sau khi DOM đã tải xong
+      if (typeof window !== 'undefined') {
+        const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+        const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+      }
+    }, []);
+
     return(
         <>
             <nav class="navbar navbar-expand-lg bg-dark " data-bs-theme="dark" >
@@ -12,30 +46,26 @@ export default function HeaderUser(){
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                   <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                      <a class="nav-link active" aria-current="page" href="#">Trang Chủ</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                      <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dạng Phim
-                      </a>
-                      <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Phim Dài Tập</a></li>
-                        <li><a class="dropdown-item" href="#">Phim Ngắn Tập</a></li>
-                      </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                      <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Top Phim
-                      </a>
-                      <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Theo Ngày</a></li>
-                        <li><a class="dropdown-item" href="#">Theo Tháng</a></li>
-                        <li><a class="dropdown-item" href="#">Theo Năm</a></li>
-                        <li><a class="dropdown-item" href="#">Theo Mùa</a></li>
-                      </ul>
+                      <a class="nav-link active" aria-current="page" href="#">TRANG CHỦ</a>
+                      <div className="segment mt-2"></div>
                     </li>
                     <li class="nav-item">
-                      <a class="nav-link " aria-current="page" href="#">Thể Loại</a>
+                      <button class="nav-link custom-tooltip" data-bs-toggle="tooltip" data-bs-placement="bottom" 
+                      data-bs-html="true" data-bs-original-title={dangphim}>
+                        DẠNG PHIM <i class="fa-solid fa-caret-down" style={{color: "#bfe661"}}></i>
+                      </button>
+                    </li>
+                    <li class="nav-item dropdown">
+                      <button class="nav-link custom-tooltip" data-bs-toggle="tooltip" data-bs-placement="bottom" 
+                      data-bs-html="true" data-bs-original-title={topphim}>
+                        TOP PHIM <i class="fa-solid fa-caret-down" style={{color: "#bfe661"}}></i>
+                      </button>
+                    </li>
+                    <li class="nav-item">
+                      <button class="nav-link custom-tooltip" data-bs-toggle="tooltip" data-bs-placement="bottom" 
+                        data-bs-html="true" data-bs-original-title={theloaiphim}>
+                          THÊ LOẠI PHIM <i class="fa-solid fa-caret-down" style={{color: "#bfe661"}}></i>
+                      </button>
                     </li>
                     <li class="nav-item">
                       <a class="nav-link " aria-current="page" href="#">Thư Viện</a>
