e0b85de2f84ed8a096aef00299fa17787ffa0b86
ameo <iamthief1412@gmail.com>
2024-09-16 14:52:19 +0700

    hello cả nhà

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -4,7 +4,9 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <>
-        
+     <div className="container-fluid bg-black text-white">
+       hello cả nhà 
+     </div>
     </>
   );
 }
