a65074b5e924d8bc48af911c101e7240ab53f43c
ameo <iamthief1412@gmail.com>
2024-09-17 15:14:11 +0700

    listchoie

--- frontend/src/app/layout/user/listchoie.jsx
+++ frontend/src/app/layout/user/listchoie.jsx
@@ -3,12 +3,12 @@ export default function ListChoie(){
         <>
             <div className="card">
               <div className="card-body">
-                <p className="card-subtitle">Hôm nay xem gì ? </p>
+                <p className="card-title">Hôm nay xem gì ? </p>
                 <div className="row">
                     <div className=" col-4 homnayxemgi mb-3 mt-3"></div>
                     <div className=" col mb-3 mt-3 homnayxemgi2"></div>
                 </div>
-                <p className="card-subtitle mb-2" style={{color:"#758792"}}>Nếu bạn buồn phiền không biết xem gì hôm nay. Hãy để chúng tôi chọn cho bạn</p>
+                <p className="card-title mb-2" style={{color:"#758792"}}>Nếu bạn buồn phiền không biết xem gì hôm nay. Hãy để chúng tôi chọn cho bạn</p>
                 <button class="btn btn-danger font-monospace" type="submit"><i class="fa-solid fa-play text-white"></i> Xem Phim <b>Ngẫu Nhiên</b> </button>
               </div>
             </div>
@@ -17,7 +17,7 @@ export default function ListChoie(){
             </div>
             <div className="card mt-3">
                 <div className="card-body">
-                    <p className="card-subtitle">PHIM MỚI CÂP NHẬT </p>
+                    <p className="card-title">PHIM MỚI CÂP NHẬT </p>
                     <div className="row">
                         <div className=" col-4 homnayxemgi mb-3 mt-3"></div>
                         <div className=" col mb-3 mt-3 homnayxemgi2"></div>
