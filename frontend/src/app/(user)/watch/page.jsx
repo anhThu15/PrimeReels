dcc45519fe4015e26df15afebd1f2577c18792b7
ameo <iamthief1412@gmail.com>
2024-09-23 16:57:51 +0700

    xem phim

--- frontend/src/app/(user)/watch/page.jsx
+++ frontend/src/app/(user)/watch/page.jsx
@@ -1,12 +1,16 @@
 'use client'
 import ListChoie from "@/app/layout/user/listchoie";
 import "../../globals.css";
-import React from 'react'
-import ReactPlayer from 'react-player'
+import ReactPlayer from 'react-player/lazy';
+import { React ,useState, useEffect } from 'react'
+import Comment from "../components/coment";
 
 
 export default function Watch(){
-  // const id = params.id
+  const [isClient, setIsClient] = useState(false)
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
 
     return(
         <>
@@ -32,10 +36,51 @@ export default function Watch(){
 
                       {/* video phim */}
                       <div className="col font-monospace text-white">
-                          <ReactPlayer url='https://youtu.be/ZfjOUs6AwM8?si=6b3yvgwgg12aOUId' controls/>
+                          {isClient ? <ReactPlayer className="w-100" style={{marginTop:"-18px"}} url='https://www.youtube.com/watch?v=L9tJjCCZ-z0' controls /> : 'Load...'}
+                          <div className="mt-3 d-flex">
+                            <button className="me-3 btn btn-btn-outline-light"><i style={{color:"#bfe661"}} class="fa-solid fa-forward"></i> Tập Tiếp Theo</button>
+                            <button className="me-3 btn btn-btn-outline-light"><i style={{color:"#bfe661"}} class="fa-solid fa-bookmark"></i> Thêm Vào Thư Viện</button>
+                            <button className="me-3 btn btn-btn-outline-light"><i style={{color:"#bfe661"}} class="fa-solid fa-rotate-left"></i> Lịch Sử Xem</button>
+                            <button className="me-3 btn btn-btn-outline-light"><i style={{color:"#bfe661"}} class="fa-solid fa-comment"></i> Bình Luận</button>
+                          </div>
                       </div>
                       {/* video phim */}
 
+
+                      {/* load tập phim  */}
+                      <div className="card mt-3">
+                        <div className="card-body">
+                          <p className="card-title">Tập Phim: </p>
+                          <div className=" w-100 d-flex flex-wrap">
+                              <button className="btn btn-danger me-2 mt-2">01</button>
+                              <button className="btn btn-outline-danger me-2 mt-2">02</button>
+                              <button className="btn btn-outline-danger me-2 mt-2">03</button>
+                              <button className="btn btn-outline-danger me-2 mt-2">04</button>
+                              <button className="btn btn-outline-danger me-2 mt-2">05</button>
+                              <button className="btn btn-outline-danger me-2 mt-2">06</button>
+                              <button className="btn btn-outline-danger me-2 mt-2">07</button>
+                              <button className="btn btn-outline-danger me-2 mt-2">08</button>
+                              <button className="btn btn-outline-danger me-2 mt-2">09</button>
+                              <button className="btn btn-outline-danger me-2 mt-2">10</button>
+                              <button className="btn btn-outline-danger me-2 mt-2">11</button>
+                              <button className="btn btn-outline-danger me-2 mt-2">12</button>
+                              <button className="btn btn-outline-danger me-2 mt-2">13</button>
+                              <button className="btn btn-outline-danger me-2 mt-2">14</button>
+                              <button className="btn btn-outline-danger me-2 mt-2">15</button>
+
+                          </div>
+                        </div>
+                      </div>
+                      {/* load tập phim  */}
+
+
+                        {/* cmt */}
+                          <div className="col font-monospace text-white">
+                            <div className="col">
+                                <Comment></Comment>
+                            </div>
+                        </div>
+                        {/* cmt */}             
 
 
                       </div>
