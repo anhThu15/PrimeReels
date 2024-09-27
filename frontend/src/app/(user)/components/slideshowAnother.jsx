c2bd9c8ae4bcab837895cb30dd6bc44520db89b4
ameo <iamthief1412@gmail.com>
2024-09-27 18:07:13 +0700

    home done

--- frontend/src/app/(user)/components/slideshowAnother.jsx
+++ frontend/src/app/(user)/components/slideshowAnother.jsx
@@ -1,27 +1,28 @@
 import Card from "./cards";
+import Film from "./film";
 
 
-export default function SlideShow(){
+export default function SlideShowAnother(){
     return (
         <>
          <div className="font-monospace pb-3 mt-3"   data-bs-theme="dark" >
-            <div id="carouselExample" class="carousel slide">
+            <div id="carouselExample2" class="carousel slide">
               <div class="carousel-inner">
                 <div class="carousel-item active ms-5">
-                  <Card></Card>
+                  <Film></Film>
                 </div>
                 <div class="carousel-item">
-                  <Card></Card>
+                  {/* <Card></Card> */}
                 </div>
                 <div class="carousel-item">
-                  <Card></Card>
+                  {/* <Card></Card> */}
                 </div>
               </div>
-              <button class="carousel-control-prev bg-white" style={{width:"50px"}} type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+              <button class="carousel-control-prev bg-white" style={{width:"50px"}} type="button" data-bs-target="#carouselExample2" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Previous</span>
               </button>
-              <button class="carousel-control-next bg-white" style={{width:"50px"}} type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+              <button class="carousel-control-next bg-white" style={{width:"50px"}} type="button" data-bs-target="#carouselExample2" data-bs-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Next</span>
               </button>
--- frontend/src/app/(user)/page.js
+++ frontend/src/app/(user)/page.js
@@ -1,6 +1,9 @@
+import Image from "next/image";
 import Banner from "./components/banner";
+import BannerAnother from "./components/bannerAnother";
 import CardSlide from "./components/cardslide";
 import SlideShow from "./components/slideshow";
+import SlideShowAnother from "./components/slideshowAnother";
 
 export default function Home() {
   
@@ -15,11 +18,29 @@ export default function Home() {
               <CardSlide></CardSlide>
           </div>
           <div >
-            <h2 className="fw-bold mb-3" style={{marginLeft:"80px"}}>Thứ 6 Ngày 13</h2>
+            <h2 className="fw-bold mb-3" style={{marginLeft:"50px"}}>Thứ 6 Ngày 13</h2>
             <SlideShow></SlideShow>
           </div>
           <div >
-            <h2 className="fw-bold mt-5" style={{marginLeft:"80px"}}>Thảm họa thiên nhiên</h2>
+            <h2 className="fw-bold mt-5" style={{marginLeft:"50px"}}>Thảm họa thiên nhiên</h2>
+            <SlideShow></SlideShow>
+          </div>
+          <div>
+            <BannerAnother></BannerAnother>
+          </div>
+          <div >
+            <h2 className="fw-bold mt-5" style={{marginLeft:"50px"}}>Rằm Trung Thu</h2>
+            <SlideShow></SlideShow>
+          </div>
+          <div className="mt-5">
+              <Image src="/images/image 23.png" width={1550} height={200}></Image>
+          </div>
+          <div >
+            <h2 className="fw-bold mt-5" style={{marginLeft:"50px"}}>Mọt Phim Tình Củm</h2>
+            <SlideShowAnother></SlideShowAnother>
+          </div>
+          <div >
+            <h2 className="fw-bold mt-5" style={{marginLeft:"50px"}}>Rằm Trung Thu</h2>
             <SlideShow></SlideShow>
           </div>
         </div>
