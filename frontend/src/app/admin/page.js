2845b9ad74c0099e2eb92f777487a603df2a322b
ameo <iamthief1412@gmail.com>
2024-10-15 14:31:06 +0700

    admin dashboard

--- frontend/src/app/admin/page.js
+++ frontend/src/app/admin/page.js
@@ -5,45 +5,57 @@ export default function Home() {
   return (
     <>
         <div className="container-fluid">
-            <div className="row pb-3">
-              <h3> <strong>Trang Quản Lý</strong> </h3>
-              <div className="col-3" data-aos="flip-left" data-aos-duration="3000">
-                <div className="card text-center">
+            <div className="row pb-3 bg-primary position-relative" style={{ height:"200px"}} >
+              <h3> <strong className=" text-white">Báo Cáo Thống Kê</strong> </h3>
+              <div className="col-3 mt-5" data-aos="flip-left" data-aos-duration="3000">
+                <div className="card text-start">
                   <div className="card-body">
-                    <p className="card-text"><i className="fa-solid fa-box fa-2xl" style={{color: "#FFD43B"}}></i></p>
-                    <h5 className="card-title">Sản Phẩm</h5>
-                    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <Link href="/admin/product" className="btn btn-warning">Quản Lý Sản Phẩm  </Link>
+                    <div className="row">
+                      <h5 className="card-title text-start col">Tổng Phim
+                        <div className="text-start mt-3 fs-1 fw-bold ">00</div>
+                      </h5>
+                      <img className="col-3" height={45} src="/images/Group 324.png"></img>
+                    </div>
+                    <div>Bộ phim đang được công chiếu</div>
                   </div>
                 </div>
               </div>
-              <div className="col-3">
-                <div className="card text-center" data-aos="flip-left" data-aos-duration="3000">
+              <div className="col-3 mt-5">
+                <div className="card text-start" data-aos="flip-left" data-aos-duration="3000">
                   <div className="card-body">
-                    <p className="card-text"><i className="fa-solid fa-list fa-2xl"></i></p>
-                    <h5 className="card-title">Danh Mục</h5>
-                    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <Link href="/admin/category" className="btn btn-warning">Quản Lý Danh Mục</Link>
+                    <div className="row">
+                        <h5 className="card-title col text-start">Tổng Danh Mục
+                          <div className="text-start mt-3 fs-1 fw-bold ">00</div>
+                        </h5>
+                        <img className="col-3" height={45} src="/images/Group 325.png"></img>
+                    </div>
+                    <div>Danh mục đang tồn tại</div>
                   </div>
                 </div>
               </div>
-              <div className="col-3">
-                <div className="card text-center" data-aos="flip-left" data-aos-duration="3000">
+              <div className="col-3 mt-5">
+                <div className="card text-start" data-aos="flip-left" data-aos-duration="3000">
                   <div className="card-body">
-                    <p className="card-text"><i className="fa-regular fa-copyright fa-2xl" style={{color: "#ff0505"}}></i></p>
-                    <h5 className="card-title">Thương Hiệu</h5>
-                    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <Link href="/admin/brand" className="btn btn-warning">Quản Lý Thương Hiệu</Link>
+                    <div className="row">
+                        <h5 className="card-title col text-start">Tổng Thể Loại
+                          <div className="text-start mt-3 fs-1 fw-bold ">00</div>
+                        </h5>
+                        <img className="col-3" height={45} src="/images/Group 326.png"></img>
+                    </div>
+                    <div>Thể loại đang tồn tại</div>
                   </div>
                 </div>
               </div>
-              <div className="col-3">
-                <div className="card text-center" data-aos="flip-left" data-aos-duration="3000">
+              <div className="col-3 mt-5">
+                <div className="card text-start" data-aos="flip-left" data-aos-duration="3000">
                   <div className="card-body">
-                    <p className="card-text"><i className="fa-solid fa-cart-shopping fa-2xl" style={{color: "#63E6BE"}}></i></p>
-                    <h5 className="card-title">Đơn Hàng</h5>
-                    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <Link href="/admin/order" className="btn btn-warning">Quản Lý Đơn Hàng</Link>
+                    <div className="row">
+                        <h5 className="card-title col text-start">Tổng Tài Khoản
+                          <div className="text-start mt-3 fs-1 fw-bold ">00</div>
+                        </h5>
+                        <img className="col-3" height={45} src="/images/Group 327.png"></img>
+                    </div>
+                    <div>Tài khoản đang hoạt động</div>
                   </div>
                 </div>
               </div>
@@ -58,7 +70,7 @@ export default function Home() {
               </div>
 
               <div className="col-md-6 pt-3"  data-aos="fade-up-left" data-aos-duration="3000">
-                <div className="card text-center">
+                <div className="card text-start">
                   <div className="card-header">
                     <b>Thông Tin Tổng Quan</b>
                   </div>
--- frontend/src/app/layout/admin/header.jsx
+++ frontend/src/app/layout/admin/header.jsx
@@ -3,18 +3,17 @@ export default function HeaderAdmin(){
         <>
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <button className="btn btn-secondary ms-1" type="button" data-bs-toggle="collapse" data-bs-target="#moMenu" aria-expanded="false" aria-controls="moMenu">
-              =
+                <i class="fa-solid fa-bars"></i>
             </button>
             <div className="container-fluid">
               <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <form className="d-flex" role="search">
-                  <input className="form-control " type="search" placeholder="Search" aria-label="Search"/>
-                  <button className="btn btn-outline-warning" type="submit">Search</button>
-                </form>
                 <ul className="navbar-nav ms-auto mb-2 me-2 mb-lg-0">
+                  <div className="nav-item rounded-circle bg-secondary mt-2" style={{width:"45px", height:"45px"}}>
+                    <i class=" fa-solid fa-bell mt-3 ms-3"></i>
+                  </div>
                   <li className="nav-item dropdown">
                     <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      Xin Chào, Anh Thư
+                      <img src="https://chontruong.edu.vn/wp-content/uploads/2024/09/meo-meme-8WUtRYq.png" className=" rounded-circle" width={45} height={45} alt="" />
                     </a>
                     <ul className="dropdown-menu ">
                       <li><a className="dropdown-item" href="#">Tìm rì ?</a></li>
