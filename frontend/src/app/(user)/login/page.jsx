b4a04766834715f7c52cb4e900617b54460e95ae
ameo <iamthief1412@gmail.com>
2024-11-21 19:56:09 +0700

    đổi push thành pathname

--- frontend/src/app/(user)/login/page.jsx
+++ frontend/src/app/(user)/login/page.jsx
@@ -14,7 +14,8 @@ export default function Login() {
     useEffect(() => {
         const token = Cookies.get('token');
         if (token) {
-            router.push("/");
+            // router.push("/");
+            window.location.pathname = '/'
         }
     }, [router]);  //kiểm trả xem đã có token chưa => đã có thì không cho vào lại trang login
 
@@ -49,9 +50,11 @@ export default function Login() {
                 // alert("mày đã xác minh rồi")
                 toast.success('Đăng nhập thành công!');
                 if (data.user.role === 100) {
-                    router.push("/administration");
+                    // router.push("/administration");
+                    window.location.pathname = '/administration'
                 } else {
-                    router.push("/");
+                    // router.push("/");
+                    window.location.pathname = '/'
                     window.location.reload();
                 }
                 
