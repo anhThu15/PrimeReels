792dae7e3fd2f524f47aa0460033e851a5e54b75
Vunewbie1407 <153430488+Vunewbie1407@users.noreply.github.com>
2024-11-27 09:42:59 +0700

    fix nextjs.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,12 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config, { isServer }) => {
-      if (!isServer) {
-        config.cache = false;
-      }
-      return config;
+    // webpack: (config, { isServer }) => {
+    //   if (!isServer) {
+    //     config.cache = false;
+    //   }
+    //   return config;
+    // },
+    env: {
+      NEXT_PUBLIC_IMAGE_URL: process.env.NEXT_PUBLIC_IMAGE_URL,
+      NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL ,
     },
+    async rewrites() {
+      return [
+        {
+          source: '/api/:path*', // `source` should start with `/`
+          destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*` // Use template literal syntax with `${}`
+        }
+      ];
+    }
   };
+  
 
 export default nextConfig;
 
