789901e66cd400ff893541d01aace98362147f24
ameo <iamthief1412@gmail.com>
2024-11-27 10:02:49 +0700

    update net config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,12 +1,36 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     webpack: (config, { isServer }) => {
+//       if (!isServer) {
+//         config.cache = false;
+//       }
+//       return config;
+//     },
+//   };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config, { isServer }) => {
-      if (!isServer) {
-        config.cache = false;
+  // webpack: (config, { isServer }) => {
+  //   if (!isServer) {
+  //     config.cache = false;
+  //   }
+  //   return config;
+  // },
+  env: {
+    NEXT_PUBLIC_IMAGE_URL: process.env.NEXT_PUBLIC_IMAGE_URL,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL ,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*', // source should start with /
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path* ` // Use template literal syntax with ${}
       }
-      return config;
-    },
-  };
+    ];
+  }
+};
+
 
 export default nextConfig;
-
